160858b9adaa8a80f0db8d4e3a051a17dcb75949
Chris Nantau <Chris.Nantau@gmail.com>
2020-08-10 17:31:00 -0300

    added a few more asserts that come up often

--- LibUnderTest.Tests/AssertionTypes.cs
+++ LibUnderTest.Tests/AssertionTypes.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace LibUnderTest.Tests
 {
@@ -21,5 +22,23 @@ namespace LibUnderTest.Tests
         {
             Assert.IsFalse(false);
         }
+
+        [TestMethod]
+        public void Asserts_OfDifferingFlavours()
+        {
+            Assert.AreEqual(0, 1 - 1);
+            Assert.IsNotNull("");
+            Assert.IsNull(null);
+
+            Assert.IsInstanceOfType(new Sibling(), typeof(Sibling));
+
+
+            Assert.IsInstanceOfType(new Sibling(), typeof(Base));
+        }
+
     }
+
+    public class Base { }
+    public class Derived : Base { }
+    public class Sibling : Base { }
 }
--- LibUnderTest/ClassUnderTest.cs
+++ LibUnderTest/ClassUnderTest.cs
@@ -2,7 +2,7 @@
 
 namespace LibUnderTest
 {
-    public class Class1
+    public class ClassUnderTest
     {
     }
 }
