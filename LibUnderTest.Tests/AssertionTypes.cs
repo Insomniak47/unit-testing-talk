516128c3d6ed1f284c1e2ba9e293d39fcfa3b711
Chris Nantau <Chris.Nantau@gmail.com>
2020-08-11 11:09:35 -0300

    Exception assert

--- LibUnderTest.Tests/AssertionTypes.cs
+++ LibUnderTest.Tests/AssertionTypes.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace LibUnderTest.Tests
 {
@@ -31,14 +32,40 @@ namespace LibUnderTest.Tests
             Assert.IsNull(null);
 
             Assert.IsInstanceOfType(new Sibling(), typeof(Sibling));
-
-
             Assert.IsInstanceOfType(new Sibling(), typeof(Base));
         }
 
+        [TestMethod]
+        public void SpecialAsserts()
+        {
+            StringAssert.Contains("wakadoo", "doo");
+            StringAssert.EndsWith("stringwithlotsofstuff", "stuff");
+            CollectionAssert.AreEqual(new List<int> { 1, 3, 5 }, new List<int> { 1, 3, 5 });
+            CollectionAssert.AllItemsAreInstancesOfType(new List<Base> { new Derived(), new Derived() }, typeof(Derived));
+            
+            //Will fail!
+            //CollectionAssert.AllItemsAreInstancesOfType(new List<Base> { new Derived(), new Sibling() }, typeof(Derived));
+        }
+
+        [TestMethod]
+        public void MakingSureThingsFail()
+        {
+            //Ensuring that ctor validation is set (Really common test type)
+            Assert.ThrowsException<ArgumentNullException>(() => new ExplodesOnNull(null));
+        }
     }
 
     public class Base { }
     public class Derived : Base { }
     public class Sibling : Base { }
+
+    public class ExplodesOnNull
+    {
+        private readonly string _special;
+
+        public ExplodesOnNull(string specialString)
+        {
+            _special = specialString ?? throw new ArgumentNullException(nameof(specialString));
+        }
+    }
 }
